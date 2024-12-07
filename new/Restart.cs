be318e11882c835b863add3ebeff2973357543ea
Lerausagi15 <zilalera80@gmail.com>
2024-12-07 22:09:35 +0300

    Update Restart.cs

--- new/Restart.cs
+++ new/Restart.cs
@@ -17,6 +17,7 @@ public class Restart : MonoBehaviour
         if(Click == true)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale = 1;
         }
     }
 
